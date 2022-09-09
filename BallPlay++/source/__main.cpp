621d8cc8aa59185a9ae7ead7d824be59a64350aa
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-09-09 19:56:32 +0200

    Hello

--- BallPlay++/source/__main.cpp
+++ BallPlay++/source/__main.cpp
@@ -1,7 +1,21 @@
+// C++
 #include <iostream>
-#include "../headers/builddate.h"
+
+// Tricky's Units
+#include <QuickString.hpp>
+
+// BallPlay++
+#include <builddate.h>
+#include <Resource.hpp>
+
+using namespace TrickyUnits;
+using namespace BallPlay;
 
 int main(int cnt, char** args) {
 	std::cout << "BallPlay ++\nBuild: " << BPP_BuildDate << "\nCoded by: Tricky\n(C) Jeroen P. Broks - Source licensed under the terms of the GPL3\n\n";
+	ResourceFile = TReplace(ExtractDir(args[0]),'\\','/') + "/BallPlay++.jcr";
+	std::cout << ID("Title") << " resource file by " << ID("Author") << "\n";
+	std::cout << "Last built: " << ID("LastBuild")<< "\n";
+	std::cout << "Build number: " << ID("Build")<< "\n";
 	return 0;
 }
