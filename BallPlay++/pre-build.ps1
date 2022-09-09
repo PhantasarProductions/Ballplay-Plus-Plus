c2ad5283d22b7e9698f3a82626be27d1c5598154
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-09-09 19:56:11 +0200

    zlib.dll

--- BallPlay++/pre-build.ps1
+++ BallPlay++/pre-build.ps1
@@ -20,3 +20,4 @@ echo "Pre-build script - (c) Jeroen P. Broks 2022"
 builddate
 ../../BuildJCR/bin/Release/net6.0/BuildJCR.exe
 
+imp "E:\projects\applications\visualstudio\vc\zlib-msvc14-x64.1.2.11.7795\build\native\bin_release\zlib.dll"
