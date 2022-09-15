61da587f97b4bbba2d15e3fb41e2afe613554120
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-09-15 20:46:28 +0200

    Suit Class

---
+++ BallPlay++/source/Game.cpp
@@ -0,0 +1,52 @@
+// Lic:
+// BallPlay++
+// Game Itself
+// 
+// 
+// 
+// (c) Jeroen P. Broks, 2022
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// Please note that some references to data like pictures or audio, do not automatically
+// fall under this licenses. Mostly this is noted in the respective files.
+// 
+// Version: 22.09.15
+// EndLic
+#include <Game.hpp>
+
+
+using namespace std;
+namespace BallPlay {
+
+	static bool __Always(Puzzle P) { return true; }
+	static bool __ShowHearts(Puzzle P) { return true; }
+	static int __Hearts(Puzzle P) { return 0; }
+	static int __Clubs(Puzzle P) { return 0; }
+	static int __Diamonds(Puzzle P) { return 0; }
+	static int __Spades(Puzzle P) { return 0; }
+
+	SClass::SClass(std::string Pic, SuitGetNum _GetNum, SuitAllow _Allow) {
+		Picture = Pic;
+		GetNum = _GetNum;
+		Allow = _Allow;
+		cout << "Initized Suit Class: " << Pic << endl;
+	}
+
+	SClass SClass::Suits[4]{
+		SClass("Hearts",__Hearts,__ShowHearts),
+		SClass("Clubs",__Clubs,__Always),
+		SClass("Diamonds",__Diamonds,__Always),
+		SClass("Spades",__Diamonds,__Always)
+	};
+}
