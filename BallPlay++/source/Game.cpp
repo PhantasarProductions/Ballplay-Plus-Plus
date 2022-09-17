f3e4391d0610dbe3804a4f8f60fbb3e09cc9f0fc
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-09-18 01:02:35 +0200

    Flow setup
    (Doesn't work yet)

--- BallPlay++/source/Game.cpp
+++ BallPlay++/source/Game.cpp
@@ -30,6 +30,8 @@
 #include <Sinus.hpp>
 #include <Game.hpp>
 #include <Fonts.hpp>
+#include <MainMenu.hpp>
+#include <Chain.hpp>
 
 
 
@@ -48,6 +50,7 @@ namespace BallPlay {
 	static TQSG_AutoImageFont Fnt{ nullptr };
 	Puzzle PlayPuzzle{ nullptr };
 
+#pragma region Kaartkleuren
 	static bool __Always(Puzzle P) { return true; }
 	static bool __ShowHearts(Puzzle P) { return PlayPuzzle->EMission() == Mission::Normal || PlayPuzzle->EMission()==Mission::ColorSplit || PlayPuzzle->EMission() == Mission::BreakFree; }
 	static int __Hearts(Puzzle P) { return 0; }
@@ -95,7 +98,121 @@ namespace BallPlay {
 		SClass("Diamonds",__Diamonds,__Always),
 		SClass("Spades",__Diamonds,__Always)
 	};
+#pragma endregion
 
+
+#pragma region Game Buttons and stages
+
+	enum class GameStages {
+		PreStart, // Just show the puzzle before move
+		Game, // Game itself
+		Pause, // Game paused
+		Success,
+		Fail
+	};
+	
+	// Choises
+	void SetGameStage(GameStages);
+	void StartPuzzle(GameStages) { Crash("Start puzzle NOT yet implemented"); }
+	void Other(GameStages); // Function needs more data, but at least I got a header now
+	void BackToMain(GameStages) { SetGameStage(GameStages::PreStart); SetChain(MainMenu); }
+	void Pause(GameStages) { SetGameStage(GameStages::Pause); }
+	void Resume(GameStages) { SetGameStage(GameStages::Game); }
+	void Next(GameStages) { Crash("Next puzzle yet to be implemented"); }
+	void Again(GameStages) { Crash("Again same puzzle yet to be implented"); }
+	void Forfeit(GameStages) { if(TQSE_Yes("Do you really want to forfeit this puzzle?")) SetGameStage(GameStages::Fail); }
+
+	// Flows
+	void EmptyFlow(GameStages) {}
+	void GameFlow(GameStages);
+	
+
+	typedef void(*StageFunction)(GameStages);
+	class _TStageButton; typedef shared_ptr<_TStageButton> TStageButton;
+	class _TStageButton {
+	public:
+		TQSG_AutoImage ButImg{nullptr};
+		StageFunction Reaction;
+		string ButFile{ "" };
+		_TStageButton(string ImgFile, StageFunction _Reaction) {
+			Reaction = _Reaction;
+			ButFile = ImgFile; // Can't load immediately as TQSG is not initized when this constructor is called.			
+		}
+	};
+	class TStage {
+	public:
+		static GameStages Current;		
+		static map<GameStages, TStage> Stage;
+		GameStages Me;
+		vector<TStageButton> Button;
+		StageFunction Flow{ nullptr };
+		TStage(GameStages id, StageFunction _Flow, vector<TStageButton> _Buttons) {
+			Me = id;
+			Flow = _Flow;
+			Button = _Buttons;
+		}
+
+		void HandleButtons() {
+			int
+				ey{ TQSG_ScreenHeight() },
+				y{ 0 },
+				mx{ TQSE_MouseX() },
+				my{ TQSE_MouseY() };
+			for (auto B : Button) {
+				if (!B->ButImg) {
+					B->ButImg = TQSG_LoadAutoImage(Resource(), "GFX/Game/Buttons/" + B->ButFile + ".png");
+					cout << "Loaded image for game button: " << B->ButFile << endl;
+				}
+				y = ey - B->ButImg->H();
+				TQSG_Color(255, 255, 255);
+				if (my > y && my < ey && mx < 5 + B->ButImg->W()) {
+					TQSG_Color(180, 0, 255);
+				}
+				B->ButImg->Draw(5, y);
+				ey = y;
+			}
+
+		}
+	};
+	GameStages TStage::Current{ GameStages::PreStart };
+	map<GameStages, TStage> TStage::Stage{
+		{ GameStages::PreStart , { GameStages::PreStart, EmptyFlow, {
+			make_shared<_TStageButton>("Main Menu", BackToMain),
+			make_shared<_TStageButton>("Other", Other),
+			make_shared<_TStageButton>("Start", StartPuzzle) }
+			} } ,
+		{ GameStages::Pause,{GameStages::Pause,EmptyFlow,{
+			make_shared<_TStageButton>("Main Menu",BackToMain),
+			make_shared<_TStageButton>("Resume",Resume) }
+			} },
+		{GameStages::Game,{GameStages::Game,GameFlow,{
+			make_shared<_TStageButton>("Forfeit",Forfeit)	,
+			make_shared<_TStageButton>("Pause",Pause)}
+			} },
+		{GameStages::Success,{GameStages::Success,EmptyFlow,{
+			make_shared<_TStageButton>("MainMenu",BackToMain),
+			make_shared<_TStageButton>("Other",Other),
+			make_shared<_TStageButton>("Again",Again),
+			make_shared<_TStageButton>("Next",Next)}
+			}},
+		{GameStages::Fail,{GameStages::Fail,EmptyFlow,{
+			make_shared<_TStageButton>("MainMenu",BackToMain),
+			make_shared<_TStageButton>("Other",Other),
+			make_shared<_TStageButton>("Again",Again)}
+			}}		
+	};
+	void SetGameStage(GameStages N) { TStage::Current = N; }
+
+#pragma endregion
+
+
+
+#pragma region Game Flow
+	void GameFlow(GameStages s){}
+#pragma endregion
+
+
+#pragma region Chain callback
 	bool Game() {
 		TQSE_Poll();
 		static int
@@ -121,9 +238,9 @@ namespace BallPlay {
 		Fnt->Draw(PlayPuzzle->MissionName(), TQSG_ScreenWidth()-2, 4,1,0);
 		TQSG_Color(180,255,0);
 		Fnt->Draw(PlayPuzzle->MissionName(), TQSG_ScreenWidth() - 4, 2, 1, 0);
-
+		TStage::Stage[TStage::Current].HandleButtons();
 		Flip();
 		return true;
 	}
-
+#pragma endregion
 }
