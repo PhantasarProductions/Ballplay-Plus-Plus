11a98e0c5ea7de06717cdb50c6a8eb478e007675
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-09-23 08:55:16 +0200

    vUpdate

--- BallPlay++/source/PuzzleSelector.cpp
+++ BallPlay++/source/PuzzleSelector.cpp
@@ -21,7 +21,7 @@
 // Please note that some references to data like pictures or audio, do not automatically
 // fall under this licenses. Mostly this is noted in the respective files.
 // 
-// Version: 22.09.22
+// Version: 22.09.23
 // EndLic
 // C++
 #include <map>
