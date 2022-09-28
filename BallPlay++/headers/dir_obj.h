6c28bd35865a2b9fa114c6bdbcc91b9b02f9721f
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-09-28 23:30:56 +0200

    vUpdate

--- BallPlay++/headers/dir_obj.h
+++ BallPlay++/headers/dir_obj.h
@@ -21,7 +21,7 @@
 // Please note that some references to data like pictures or audio, do not automatically
 // fall under this licenses. Mostly this is noted in the respective files.
 // 
-// Version: 22.09.22
+// Version: 22.09.28
 // EndLic
 #pragma once
 #ifdef __cplusplus
